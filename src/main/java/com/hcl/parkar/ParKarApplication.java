6ae6bfb68b5d9df224a55e113988375341f121c1
Pravesh Kumar <p.kumar3@dhl.com>
2022-03-08 11:16:20 +0530

    first commit

---
+++ src/main/java/com/hcl/parkar/ParKarApplication.java
@@ -0,0 +1,13 @@
+package com.hcl.parkar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParKarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParKarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hcl/parkar/ParKarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hcl.parkar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParKarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
