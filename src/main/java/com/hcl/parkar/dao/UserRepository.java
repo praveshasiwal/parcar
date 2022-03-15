122f2e00380612f973b7af9d9a8cfab36f6ae9d4
teja.jajula <teja.jajula@LP-212825112.HCLT.CORP.HCL.IN>
2022-03-15 12:24:22 +0530

    parkar : update service

--- src/main/java/com/hcl/parkar/dao/UserRepository.java
+++ src/main/java/com/hcl/parkar/dao/UserRepository.java
@@ -1,40 +1,15 @@
 package com.hcl.parkar.dao;
 
-import java.util.List;
 import java.util.Optional;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import com.hcl.parkar.model.UserEntity;
 
-public interface UserRepository extends JpaRepository<UserEntity, Long> {
-
+@Repository
+public interface UserRepository extends CrudRepository<UserEntity, Long> {
 	
-
-	
+	Optional<UserEntity> findByUserName(String userName);
 
 }
-
-//package com.hcl.parkar.dao;
-//import java.util.List;
-//
-//
-//
-//import org.springframework.data.repository.CrudRepository;
-//import org.springframework.stereotype.Repository;
-//
-//
-//
-//import com.hcl.parkar.model.UserEntity;
-//
-//
-//
-//
-//
-//@Repository
-//public interface UserRepository extends CrudRepository<UserEntity, Long> {
-//List<UserEntity> findbyid(String UserName);
-//
-//
-//}
