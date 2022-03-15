d3875908f2e863493cf41d203a38462c153cdff2
dharsana.a <dharsana.a@LP-212825076.HCLT.CORP.HCL.IN>
2022-03-15 11:25:52 +0530

    parcar : created user and vehicle entity

---
+++ src/main/java/com/hcl/parkar/model/VehicleEntity.java
@@ -0,0 +1,63 @@
+package com.hcl.parkar.model;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "vehicle")
+public class VehicleEntity implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@Column(name = "vehicle_number", nullable = false, length = 30)
+	private int vehicleNumber;
+	@Column(name = "vehicle_category", nullable = false, length = 10)
+	private String vehicleCategory;
+	@Column(name = "vehicle_city", nullable = false, length = 50)
+	private String vehicleCity;
+	@Column(name = "vehicle_State", nullable = false, length = 50)
+	private String vehicleState;
+
+	public int getVehicleNumber() {
+		return vehicleNumber;
+	}
+
+	public void setVehicleNumber(int vehicleNumber) {
+		this.vehicleNumber = vehicleNumber;
+	}
+
+	public String getVehicleCategory() {
+		return vehicleCategory;
+	}
+
+	public void setVehicleCategory(String vehicleCategory) {
+		this.vehicleCategory = vehicleCategory;
+	}
+
+	public String getVehicleCity() {
+		return vehicleCity;
+	}
+
+	public void setVehicleCity(String vehicleCity) {
+		this.vehicleCity = vehicleCity;
+	}
+
+	public String getVehicleState() {
+		return vehicleState;
+	}
+
+	public void setVehicleState(String vehicleState) {
+		this.vehicleState = vehicleState;
+	}
+
+	@Override
+	public String toString() {
+		return "VehicleEntity [vehicleNumber=" + vehicleNumber + ", vehicleCategory=" + vehicleCategory
+				+ ", vehicleCity=" + vehicleCity + ", vehicleState=" + vehicleState + "]";
+	}
+
+}
